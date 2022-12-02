24a7a245a4be77d422c88b033b59def5d5317209
Karolina Dbaj <k.dbaj@vp.pl>
2022-12-02 19:01:58 +0100

    react components

--- frontend/src/components/notes/toDoList.js
+++ frontend/src/components/notes/toDoList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Notes.css";
 import Note from "../Note/Note";
+import NewNote from "../NewNote/NewNote";
+import EditNote from "../EditNote/EditNote";
+import Modal from "react-modal";
 
 class ToDoList extends React.Component {
   constructor(props) {
@@ -19,13 +22,42 @@ class ToDoList extends React.Component {
           body: "Wyprowadzić psa i wykąpać po spacerze",
         },
       ],
+      showEditModal: false,
+      editNote: {},
     };
   }
 
+  //usuwanie notatki
   deleteNote(id) {
     console.log("usuwanie notatki", id);
     const notes = [...this.state.notes].filter((note) => note.id !== id);
     this.setState({ notes });
+  }
+
+  //usuwanie notatki
+  addNote(note) {
+    const notes = [...this.state.notes];
+    notes.push(note);
+    this.setState({ notes });
+  }
+
+  editNote(note) {
+    const notes = [...this.state.notes];
+    const index = notes.findIndex((x) => x.id === note.id);
+    if (index >= 0) {
+      notes[index] = note;
+      this.setState({ notes });
+    }
+    this.toggleModal();
+  }
+
+  toggleModal() {
+    this.setState({ showEditModal: !this.state.showEditModal });
+  }
+
+  editNoteHandler(note) {
+    this.toggleModal();
+    this.setState({ editNote: note });
   }
 
   render() {
@@ -35,12 +67,26 @@ class ToDoList extends React.Component {
         <header>
           <p>Moje notatki:</p>
         </header>
+
+        <NewNote onAdd={(note) => this.addNote(note)} />
+
+        <Modal isOpen={this.state.showEditModal} contentLabel="Edytuj notatkę">
+          <EditNote
+            title={this.state.editNote.title}
+            body={this.state.editNote.body}
+            id={this.state.editNote.id}
+            onEdit={(note) => this.editNote(note)}
+          />
+          <button onClick={() => this.toggleModal()}>Anuluj</button>
+        </Modal>
+
         {this.state.notes.map((note) => (
           <Note
             key={note.id}
             title={note.title}
             body={note.body}
             id={note.id}
+            onEdit={(note) => this.editNoteHandler(note)}
             onDelete={(id) => this.deleteNote(id)}
           />
         ))}
